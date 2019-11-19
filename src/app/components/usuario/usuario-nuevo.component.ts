34dc267a45c1ed7d3b7f674e47283320e3f7b195
Roberto Agirre <r.agirre@ibermatica.com>
2019-11-19 16:58:14 +0100

    Refactor Rutas hijas

--- src/app/components/usuario/usuario-nuevo.component.ts
+++ src/app/components/usuario/usuario-nuevo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-usuario-nuevo',
@@ -11,7 +12,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuarioNuevoComponent implements OnInit {
 
-  constructor() { }
+  constructor( private route: ActivatedRoute) {
+    this.route.parent.params.subscribe( parametros => {
+      console.log('Ruta HIJA NUEVO');
+      console.log(parametros);
+    });
+ }
 
   ngOnInit() {
   }
--- src/app/components/usuario/usuario.component.ts
+++ src/app/components/usuario/usuario.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-usuario',
   templateUrl: './usuario.component.html'
 })
 export class UsuarioComponent implements OnInit {
 
-  constructor() { }
+  constructor( private route: ActivatedRoute) {
+      this.route.params.subscribe( parametros => {
+        console.log('Ruta PADRE');
+        console.log(parametros);
+      });
+   }
 
   ngOnInit() {
   }
